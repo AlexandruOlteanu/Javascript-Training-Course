c4292d006570adcb972fe9782b1519148e53f5e1
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-10-17 10:34:43 +0300

    Finished Adding Dogs

--- Lab05/script.js
+++ Lab05/script.js
@@ -1,29 +1,17 @@
-fetch("http://localhost:3000/dogs")
-    .then(
+let addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addDog);
 
-        function(response) {
-            if (response.status !== 200) {
-                console.log("Error with code " + response.status);
-            }
-            else {
-                response.json().then(function(data) {
 
-                    var len = data.length;
-                    data.forEach(element => {
-                        let img = document.createElement("img");
-                        img.src = element.img;
-                        let paragraph = document.createElement("p");
-                        paragraph.setAttribute("id", element.name.toLowerCase());
-                        paragraph.innerText = element.name;
-                        paragraph.style.fontSize = "30px";
-                        document.body.appendChild(img);
-                        document.body.appendChild(paragraph);
-                    
-                    });
 
-                });
-
-            }
-        }
-
-    )
+function addDog() {
+    let name = document.getElementById("name").value;
+    let imageUrl = document.getElementById("image_url").value;
+    let img = document.createElement("img");
+    img.src = imageUrl;
+    let paragraph = document.createElement("p");
+    paragraph.setAttribute("id", name.toLowerCase());
+    paragraph.innerText = name;
+    paragraph.style.fontSize = "30px";
+    document.body.appendChild(img);
+    document.body.appendChild(paragraph);
+}
