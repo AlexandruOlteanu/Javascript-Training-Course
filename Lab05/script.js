5eb2766f42c01b8b2144f869a792c87fdce7b200
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-10-17 10:43:54 +0300

    Progress Made

--- Lab05/script.js
+++ Lab05/script.js
@@ -1,8 +1,7 @@
 let addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addDog);
 
-
-
+let id = 1;
 function addDog() {
     let name = document.getElementById("name").value;
     let imageUrl = document.getElementById("image_url").value;
@@ -14,4 +13,11 @@ function addDog() {
     paragraph.style.fontSize = "30px";
     document.body.appendChild(img);
     document.body.appendChild(paragraph);
+    let postRequest = new XMLHttpRequest();
+    // postRequest.open("POST", "http://localhost:3000/dogs");
+    // let data = {
+    //     "name" : name,
+    //     "img" : imageUrl
+    // }
+    // postRequest.send(data);
 }
